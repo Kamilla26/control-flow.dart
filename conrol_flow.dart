737ec61bf12469a4702293f09456659b1364750d
unknown <omolloashley2@gmail.com>
2024-03-07 11:08:56 +0300

    first commit

---
+++ conrol_flow.dart
@@ -0,0 +1,12 @@
+void main() {
+  var number = 15;
+  if (number > 10) {
+    print("Your number is greater than 10");
+  }
+  if (number < 10) {
+    print("Your number is less than 10");
+  }
+  if (number == 10) {
+    print("Your number is equal to 10");
+  }
+}
